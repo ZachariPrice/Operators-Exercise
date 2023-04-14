0a5ed9e3a5744fc6de2db8ae3ebf353cd223084e
Zachari Price <ZachariPrice3@gmail.com>
2023-04-14 17:42:00 -0400

    Operators-exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace OperatorExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+
+
+            Console.WriteLine($"{a} / {b} {quotient} remainder {remainder}");
+
+
+            Console.WriteLine("What is the radius of your cricle?");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine (AreaOfCircle(radius));
+
+           
+
         }
-    }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+    }    
 }
+
+
